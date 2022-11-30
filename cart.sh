59b36f9ab0508676920482a2395a4a88a03a27dd
Ayobami O <ayobamiojeniyi@yahoo.com>
2022-11-29 22:50:42 -0600

    MySQL Script

--- cart.sh
+++ cart.sh
@@ -1,18 +1,49 @@
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
-useradd roboshop
+source common.sh
 
+PRINT "Install NodeJS Repos"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+STAT $?
+
+PRINT "Install NodeJS"
+yum install nodejs -y
+STAT $?
+
+PRINT "Adding Application User"
+useradd roboshop
+STAT $?
+
+PRINT "DOWNLOAD App Content"
 curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"
+STAT $?
+
+PRINT "Remove Previous Version of App"
 cd /home/roboshop
 rm -rf cart
+STAT $?
+
+PRINT "Extracting App Content"
 unzip -o /tmp/cart.zip
+STAT $?
+
 mv cart-main cart
 cd cart
+
+PRINT "Install NodeJS Dependencies for App"
 npm install
+STAT $?
 
+PRINT "Configure Endpoints for SystemD Configuration"
 sed -i -e 's/REDIS_ENDPOINT/redis.devopsb53.online/' -e 's/CATALOGUE_ENDPOINT/catalogue.devopsb53.online/' /home/roboshop/cart/systemd.service
+STAT $?
 
-mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service
-systemctl daemon-reload
+PRINT "Reload SystemD"
 systemctl restart cart
+STAT $?
+
+PRINT "Restart Cart"
+systemctl restart cart
+STAT $?
+
+PRINT "Enable Cart Service"
 systemctl enable cart
+STAT $?
