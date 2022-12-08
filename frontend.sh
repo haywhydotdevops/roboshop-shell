0a0b8fd773d27af92238e27c81e5ed1765c21d01
Ayobami O <ayobamiojeniyi@yahoo.com>
2022-12-08 11:47:24 -0600

    MySQL Script

--- frontend.sh
+++ frontend.sh
@@ -16,6 +16,9 @@ PRINT "Copy RoboShop Configuration File"
 mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
 STAT $?
 
+PRINT "Update RoboShop Configuration"
+sed -i -e '/catalogue/ s/localhost/catalogue-dev.devopsb53.online/' -e '/user/ s/localhost/user-dev.devopsb53.online/' -e '/cart/ s/localhost/cart-dev.devopsb53.online/' -e '/shipping/ s/localhost/shipping-dev.devopsb53.online/' -e '/payment/ s/localhost/payment-dev.devopsb53.online/'  /etc/nginx/default.d/roboshop.conf
+STAT $?
 
 PRINT "Enable Nginx Service"
 systemctl enable nginx &>>$LOG
